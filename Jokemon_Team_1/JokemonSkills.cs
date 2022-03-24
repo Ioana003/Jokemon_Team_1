e22bd3f86224af50a68fd14e55621b1be8d7d246
Charles Zhizheng-Ting <10Zhizheng-TingC@student.smchull.org>
2022-03-24 10:16:24 +0000

    juih

--- Jokemon_Team_1/JokemonSkills.cs
+++ Jokemon_Team_1/JokemonSkills.cs
@@ -6,8 +6,20 @@ namespace Jokemon_Team_1
 {
     class JokemonSkills
     {
-        public int healthdecrease;
+        public int healthdecrease { get; set; }
+        //where types of skills are stored
         public JokemonSkills()
+        {
+            //constructor
+        }
+
+        public int IronTail()  // pika a chu skill
+        {
+            healthdecrease = 10;
+            return healthdecrease;
+        }
+
+        public void Nuzzle()   // pika a chu skill
         {
 
         }
