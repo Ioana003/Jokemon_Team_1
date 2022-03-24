dd32119815046f5831ee071371f6e4427f377881
Charles Zhizheng-Ting <10Zhizheng-TingC@student.smchull.org>
2022-03-24 09:58:47 +0000

    selfuh

---
+++ Jokemon_Team_1/JokemonSkills.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jokemon_Team_1
+{
+    class JokemonSkills
+    {
+        public int healthdecrease;
+        public JokemonSkills()
+        {
+
+        }
+    }
+}
